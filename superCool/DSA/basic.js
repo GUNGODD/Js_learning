d0635d6e47d79ca82d8e761d1ed747803fa9f2a4
GUNGODD <chauhanadityac01@gmail.com>
2023-12-25 11:57:52 +0530

    "Added day 2 streak"

---
+++ superCool/DSA/basic.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = 33;
+let c = 23;
+if ( b  == c ){
+    console.log("equal");
+}
+else {
+    console.log("not equal");
+}
---
+++ superCool/recursion.js
@@ -0,0 +1,15 @@
+/*
+Shift to obtain the array [4,3,2,1,5
+];
+Shift to obtain the array [5,4,3,2,1
+];
+Reverse to obtain the array [1,2,3,4,5
+].
+*/
+
+function clear(arr){
+    let arr = [5,4,3,2,1,7,8,9] // length = 9/gap 
+
+
+}
+let arr = [4,3,2,1,5];
